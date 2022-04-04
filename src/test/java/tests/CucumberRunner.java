6967cd926955a267814451c22ef76cc8c76f71f5
ArtemShDev <dev.art.balt@gmail.com>
2022-04-04 12:17:43 +0300

    debug #3 Modify

--- src/test/java/tests/CucumberRunner.java
+++ src/test/java/tests/CucumberRunner.java
@@ -15,4 +15,5 @@ import org.junit.runner.RunWith;
 public class CucumberRunner {
     // string for debug_code branch
     // string for debug_code branch # 2
+    // string for debug_code branch # 3 Modify
 }
