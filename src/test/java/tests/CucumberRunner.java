f47f59a440f56d865a8d26c22817861b84c2cfec
ArtemShDev <dev.art.balt@gmail.com>
2022-04-04 13:32:04 +0300

    debug #test

--- src/test/java/tests/CucumberRunner.java
+++ src/test/java/tests/CucumberRunner.java
@@ -16,4 +16,5 @@ public class CucumberRunner {
     // string for debug_code branch
     // string for debug_code branch # 2
     // string for debug_code branch # 4 Modify_3
+    // test for test
 }
